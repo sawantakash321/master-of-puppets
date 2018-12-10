82fd9b692c0a49afdd8405f55bb02d089fea6eac
Akash Sawant <sawantakash321@gmail.com>
2018-12-11 01:56:33 +0530

    Updated stateless NotFound  component

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.scss';
+
+const NotFound = function() {
+  return (
+    <div className="not-found">
+      <h1>Not Found 404</h1>
+    </div>
+  );
+};
+
+export default NotFound;
