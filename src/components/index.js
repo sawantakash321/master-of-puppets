341289f0eb272f6e5a22b01fa726fe4beb3af4c1
Akash Sawant <sawantakash321@gmail.com>
2018-12-11 01:46:26 +0530

    Added components file structure

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import SkillEditor from './SkillEditor/SkillEditor';
+import NotFound from './NotFound/NotFound';
+import Select from './Select/Select';
+import Skills from './Skills/Skills';
+
+export { SkillEditor, NotFound, Select, Skills };
